5f19151656d9d6f7d168b989ff3764d52a272482
markmccutch <mark.mccutcheon29@gmail.com>
2019-11-26 15:46:05 -0500

    working through a matrix version that works for a 2 variable problem like [p,v], but uses matrices so that computations are done nicely with capacity to be expanded to N dimensions in the future

--- Kalman.cpp
+++ Kalman.cpp
@@ -3,28 +3,45 @@ Authored By
 Mark McCutcheon, Clutch Design Solutions
 Gitub: https://github.com/markmccutch
 Email: mark.mccutcheon29@gmail.com
+
+Dependencies:
+MatrixMath: https://github.com/eecharlie/MatrixMath
+
 */
 
 #include "Kalman.h"
-
+#include "MatrixMath.h"
 
 
 //Constructor
-Kalman::Kalman(float position_0, float velocity_0, float U, float P_0, float Q, float R, float H) {
-  _position = position_0;
-  _velocity = velocity_0;
-  _lastPosition = _position;
-  _lastVelocity = _velocity;
-  _u = U;
-  _initialP = P_0;
-  _Q = Q;
-  _R = R;
-  _H = H;
+Kalman::Kalman(float position_0, float velocity_0, float P_0, float Q, float R, float H) {
+  _position_velocity[0] = position_0;
+  _position_velocity[1] = velocity_0;
+  _lastPosition = position_0;
+  _lastVelocity = velocity_0;
+  _u = 0;
+  _y = 0;
 
-  _P[0][0] = _initialP;
-  _P[0][1] = 0.0f;
-  _P[1][0] = 0.0f;
-  _P[1][1] = _initialP;
+  //fill Matrices with diagonal matrices with the required values
+  for (int i =0; i<2; i++) {
+    for (int j = 0; j<2; j++) {
+      if( i == j) {
+        _Q[i][j] = Q;
+        _R[i][j] = R;
+        _H[i][j] = H;
+        _P[i][j] = P_0;
+        _S[i][j] = 0;
+        _K[i][j] = 0;
+      } else {
+        _Q[i][j] = 0;
+        _R[i][j] = 0;
+        _H[i][j] = 0;
+        _P[i][j] = 0;
+        _S[i][j] = 0;
+        _K[i][j] = 0;
+      }
+    }
+  }
 };
 
 
@@ -53,5 +70,61 @@ void setEstimatedCovariance(float cov[2][2]){
 
 //setProcessError
 void setProcessError(float err_q){
-  this->_Q =
+  this->_Q[0][0] = err_q;
+  this->_Q[0][1] = 0;
+  this->_Q[1][0] = 0;
+  this->_Q[1][1] = err_q;
 };
+
+//TODO: getProcessError
+
+//setSensorError
+void setSensorError(float err_r){
+  this->_R[0][0] = err_r;
+  this->_R[0][1] = 0;
+  this->_R[1][0] = 0;
+  this->_R[1][1] = err_r;
+};
+
+//TODO: getSensorError
+
+//getInnovation
+float getInnovation(){
+   return this->_y;
+};
+
+//TODO: getInnovationCovariance
+
+//TODO: getKalmanGain
+
+//TODO: getEstimateError
+
+//getPosition
+float getPosition() {
+  return this->_position_velocity[0];
+}
+
+//getVelocity
+float getVelocity(){
+  return this->_position_velocity[1];
+}
+
+
+// Calculations -------------------------
+
+//updateEstimate
+void predictEstimate(float u, int dt){
+
+  //Transfer state to old state storage
+  _lastPosition = _position_velocity[0];
+  _lastVelocity = _position_velocity[1];
+  //PREDICT
+  //maintain values
+  _position = _lastPosition + _lastVelocity*dt + 0.5*dt*dt*_u;
+  _velocity = _lastVelocity + dt*_u;
+  //maintain covariance
+
+
+
+
+}
--- Kalman.h
+++ Kalman.h
@@ -15,7 +15,8 @@ public:
     float position_0, float velocity_0, float U, float P_0, float Q, float R, float H
   );
 
-  void updateEstimate(float measured_velocity, float measured_position, float dt);
+  void predictEstimate(float u, int dt);
+  void updateEstimate(float measured_velocity, float measured_position, int dt);
 
   void setControlVector(float u_vect);
   void setEstimatedCovariance(float cov[2][2]);
@@ -24,28 +25,33 @@ public:
 
   float getControlVector();
   //TODO: float getEstimatedCovariance();
-  float getProcessError();
-  float getSensorError();
+  //TODO: float getProcessError();
+  //TODO: float getSensorError();
 
-  float getInnovation();
-  float getInnovationCovariance();
-  float getKalmanGain();
-  float getEstimateError();
+  //TODO: float getInnovation();
+  //TODO: float getInnovationCovariance();
+  //TODO: float getKalmanGain();
+  //TODO: float getEstimateError();
 
   float getPosition();
   float getVelocity();
 
 private:
-  float _position;
-  float _velocity;
+  float _position_velocity[2];
   float _lastPosition;
   float _lastVelocity;
-  float _u;
-  float _initialP;
-  float _Q[2][2];
-  float _R[2][2];
-  float _H[2][2];
-  float _P[2][2];
+  float _u; //control vector (Acceleration)
+  float _y; //innovation
+
+  float _Q[2][2]; //Process Error (Accelerometer reading variance)
+  float _R[2][2]; //Sensor Error (GPS reading variance)
+  float _H[2][2]; //Input Translation Matrix (2X2diag(1))
+  float _P[2][2]; //Estimated Covariance
+  float _S[2][2]; //innovation covarience
+  float _K[2][2]; //Kalman Gain
+
+  float _A[2][2];
+  float _B[2][2];
 
 };
 
